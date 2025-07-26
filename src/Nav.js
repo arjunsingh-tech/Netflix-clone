8fae6884c0aab7ab876c0546250fd119c7ebc07b
Arjun Thakur <105769952+arjunsinghjadaun@users.noreply.github.com>
2025-07-26 18:20:25 +0530

    --error resolved

--- src/Nav.js
+++ src/Nav.js
@@ -1,30 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import './Nav.css'
 
-
 function Nav() {
     const [show, handleShow] = useState(false);
 
     useEffect(() => {
-        window.addEventListener('scroll', () => {
+        // Store the function reference
+        const handleScroll = () => {
             if (window.scrollY > 100) {
                 handleShow(true);
-            } else handleShow(false);
-        });
+            } else {
+                handleShow(false);
+            }
+        };
+
+        // Add event listener with the stored function
+        window.addEventListener('scroll', handleScroll);
+
+        // Cleanup: remove event listener with both arguments
         return () => {
-            window.removeEventListener('scroll');
+            window.removeEventListener('scroll', handleScroll);
         };
     }, []);
-    // show the scrolling black strip form  
+
     return (
         <div className={`nav ${show && 'nav__black'}`}>
-            <img class="nav_logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix"/>
-
-            <img class="nav__avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Netflix Logo"></img>
-            
+            <img className="nav_logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix"/>
+            <img className="nav__avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Netflix Avatar"/>
         </div>
     )
 }
 
 export default Nav
-
